fa49d2d0c04efae8440dc20367753c0e3d8f291e
Angelleyva-60876 <71714746+Angelleyva-60876@users.noreply.github.com>
2022-03-08 06:59:29 -0600

    view

--- Main.py
+++ Main.py
@@ -1,1 +1,19 @@
-print("hola")
+import sys
+
+from view.view_winmain import Ui_Form
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore,QtGui,QtWidgets
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.view = Ui_Form()
+        self.view.setupUi(self)
+        
+        
+    
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec_()
